652e77fad6aa3a32c29e0abade47c435204937c8
Hara Keishi <hara.for.work@gmail.com>
2019-03-08 14:11:42 +0900

    ログイン用APIのルーティング情報を記述

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 // 会員登録API
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
+// ログイン
+Route::post('/login', 'Auth\LoginController@login')->name('login');
