5524dc24d0110ada9addc24aeb99eca45f9cb595
Hara Keishi <hara.for.work@gmail.com>
2019-03-08 13:31:45 +0900

    VueRouterの導入と２ページのルーティング情報注入

---
+++ resources/js/pages/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="Login">
+    <h1>Login</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style scoped>
+</style>
---
+++ resources/js/pages/topPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="TopPage">
+    <h1>TopPage</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopPage'
+}
+</script>
+
+<style scoped>
+</style>
