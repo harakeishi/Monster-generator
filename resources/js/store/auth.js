22b406382db65cd83691f1b1a1ac644d9b966bc7
Hara Keishi <hara.for.work@gmail.com>
2019-03-13 13:56:28 +0900

    ログイン時のフロント側セッション保持の設定２

--- resources/js/store/auth.js
+++ resources/js/store/auth.js
@@ -2,7 +2,10 @@ const state = {
   user: null
 }
 
-const getters = {}
+const getters = {
+  check: state => !! state.user,// loginチェック
+  username: state => state.user ? state.user.name : ''
+}
 
 const mutations = {
   setUser (state, user) {
@@ -18,6 +21,15 @@ const actions = {
   async login (context, data) {
     const response = await axios.post('/api/login', data)
     context.commit('setUser', response.data)
+  },
+  async logout (context) {
+    const response = await axios.post('/api/logout')
+    context.commit('setUser', null)
+  },
+  async currentUser (context) {
+    const response = await axios.get('/api/user')
+    const user = response.data || null
+    context.commit('setUser', user)
   }
 }
 
